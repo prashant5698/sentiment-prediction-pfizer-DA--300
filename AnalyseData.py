b5683e22869170eac8b0faa78c603546c65f4521
tabishqidwai98 <60247314+tabishqidwai98@users.noreply.github.com>
2021-04-30 18:03:37 +0530

    wrote nicely

--- AnalyseData.py
+++ AnalyseData.py
@@ -14,8 +14,11 @@ class Analyse:
                                                                        ('strong' if 10000>=x>1000
                                                                         else 'influencer')))
         self.df['total_engagement']=self.df['retweets']+self.df['favorites']
+
         self.df['med'] = self.df['text'].apply(lambda word:word.count('https://t.co/'))
         self.df['med'] = self.df['med'].apply(lambda x:'No Media' if x==0 else 'Media')
+
         L = ['year', 'month', 'day', 'dayofweek', 'dayofyear', 'weekofyear', 'quarter']
         self.df = self.df.join(pd.concat((getattr(self.df['date'].dt, i).rename(i) for i in L), axis=1))
+        
         return self.df
